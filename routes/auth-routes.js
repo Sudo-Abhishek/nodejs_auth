b5e653f32fa0143da01613144c8b02efc628eed4
Sudo-Abhishek <abhishek.agni2498@gmail.com>
2025-10-05 23:41:24 +0530

    typo fixes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 // all routes related to authentication and authorization
 router.post("/register", registerUser);
-router.get("/login", loginUser);
+router.post("/login", loginUser);
 router.post("/change-password", authMiddleware, changePassword);
 
 module.exports = router;
